5608c07ad412df4d73ab29f2b39322ce6691a54e
Petr Afanasyev <44192523+apmfiit@users.noreply.github.com>
2019-11-08 22:35:48 +0900

    Задание №2: fibonacci1

--- hw1/fibonacci.go
+++ hw1/fibonacci.go
@@ -5,15 +5,21 @@ import (
 )
 
 func fibonacci1(n int) int {
+
 	a := []int{0, 1}
 
 	for i := 2; i <= n; i++ {
+
 		a = append(a, a[i - 1] + a[i - 2])
+
 	}
 
 	return a[n]
+
 }
 
 func main() {
+	
 	fmt.Println(fibonacci1(7))
+	// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 ..
 }
